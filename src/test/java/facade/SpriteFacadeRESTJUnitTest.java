bd802686f9a091281cac95e1306caa47965808b1
Hoang Do <katievincy226@gmail.com>
2022-04-05 18:02:37 -0400

    Add unit test

---
+++ src/test/java/facade/SpriteFacadeRESTJUnitTest.java
@@ -0,0 +1,61 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package facade;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ *
+ * @author Khanh Do
+ */
+public class SpriteFacadeRESTJUnitTest {
+    
+    public SpriteFacadeRESTJUnitTest() {
+    }
+
+    @org.junit.jupiter.api.BeforeAll
+    public static void setUpClass() throws Exception {
+    }
+
+    @org.junit.jupiter.api.AfterAll
+    public static void tearDownClass() throws Exception {
+    }
+
+    @org.junit.jupiter.api.BeforeEach
+    public void setUp() throws Exception {
+    }
+
+    @org.junit.jupiter.api.AfterEach
+    public void tearDown() throws Exception {
+    }
+    
+    @BeforeAll
+    public static void setUpClassBefore() {
+    }
+    
+    @AfterAll
+    public static void tearDownClassAfterAll() {
+    }
+    
+    @BeforeEach
+    public void setUpBeforeEach() {
+    }
+    
+    @AfterEach
+    public void tearDownAfterEach() {
+    }
+
+    // TODO add test methods here.
+    // The methods must be annotated with annotation @Test. For example:
+    //
+    // @Test
+    // public void hello() {}
+}
