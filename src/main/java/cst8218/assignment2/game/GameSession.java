b043506e0e58b28897b60ce3832c8ec6a76872fb
Hoang Do <katievincy226@gmail.com>
2022-04-09 16:48:09 -0400

    Added game session test

--- src/main/java/cst8218/assignment2/game/GameSession.java
+++ src/main/java/cst8218/assignment2/game/GameSession.java
@@ -10,6 +10,10 @@ import java.util.List;
 import javax.ejb.Stateless;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import javax.persistence.TypedQuery;
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Root;
 
 
 @Stateless
@@ -22,10 +26,24 @@ public class GameSession {
         return em;
     }
     
+    public void setEntityManager(EntityManager em){
+        this.em = em;
+    }
+    
+    
+    
     public List<Sprite> findAll(){
-        javax.persistence.criteria.CriteriaQuery eq = getEntityManager().getCriteriaBuilder().createQuery();
-        eq.select(eq.from(Sprite.class));
-        return getEntityManager().createQuery(eq).getResultList();
+//        javax.persistence.criteria.CriteriaQuery eq = getEntityManager().getCriteriaBuilder().createQuery();
+//        eq.select(eq.from(Sprite.class));
+//        return getEntityManager().createQuery(eq).getResultList();
+        
+        CriteriaBuilder cb = getEntityManager().getCriteriaBuilder();
+        CriteriaQuery eq1 = cb.createQuery();
+        Root rootEntry = eq1.from(Sprite.class);
+        eq1.select(rootEntry);
+        TypedQuery<Sprite> allQuery = em.createQuery(eq1);
+        List<Sprite> sprites = allQuery.getResultList();
+        return sprites;
     }
     
     public void create(Sprite entity) {
